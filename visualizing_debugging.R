5c9a5b81ed5f10f28c508c4216b03044d0b176b3
Ian <ian.mellis@gmail.com>
2021-11-09 10:31:38 -0500

    setting up for latin hypercube sampling applied to prototype

--- visualizing_debugging.R
+++ visualizing_debugging.R
@@ -67,4 +67,31 @@ burstOn_plot <- ggplot() +
   ylab('Burst status')
 grid.arrange(spec_plot, burstOn_plot, ncol=1)
 
+results <- t(as.matrix(read.csv('S_outpar_test_ifs_nonsenseSpecies2.csv', header=F)))
+colnames(results) <- c('orig_1', 'nonsense_1', 'para_1', 'targ_1', 'targ_off', 'targ_on', 'para_off', 'para_on', 'orig_on', 'orig_off', 'is_mutated', 'not_mutated')
+results %<>% as.data.frame() %>% as_tibble()
+results[,'time'] <- 1:nrow(results)
 
+orig_color = 'black'
+nons_color = 'orange'
+para_color = 'gray'
+targ_color = 'blue'
+t_start = 9900
+nt = 300
+spec_plot <- ggplot() +
+  theme_classic() +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, orig_1), color = orig_color) +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, nonsense_1), color = nons_color) +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, para_1), color = para_color) +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, targ_1), color = targ_color) +
+  ylab('Abundance')
+
+burstOn_plot <- ggplot() +
+  theme_classic() +
+  geom_line(data = results[t_start:10000,], aes(time, orig_on + 0.1), color = orig_color) +
+  geom_line(data = results[10000:(t_start+nt),], aes(time, orig_on + 0.1), color = nons_color) +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, para_on + 0.05), color = para_color) +
+  geom_line(data = results[t_start:(t_start+nt),], aes(time, targ_on + 0), color = targ_color) +
+  geom_vline(data = results[t_start:(t_start+nt),], aes(xintercept = 10000), color = nons_color, linetype = 2) +
+  ylab('Burst status')
+grid.arrange(spec_plot, burstOn_plot, ncol=1)
