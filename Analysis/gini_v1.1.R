99f68cc9dabefeee333ee4d84cfef4eb4f34b323
Ian <ian.mellis@gmail.com>
2021-11-10 16:10:42 -0500

    outline of analysis

--- Analysis/gini_v1.1.R
+++ Analysis/gini_v1.1.R
@@ -9,21 +9,30 @@ setwd('~/code/grn_nitc/nitc_3node_v1.1/')
 # load and process data
 n_sets <- 100
 for (i in 1:n_sets){
-results <- t(as.matrix(read.csv('S_outpar_test_ifs.csv', header=F)))
-colnames(results) <- c('orig_1', 'para_1', 'targ_1', 'targ_off', 'targ_on', 'para_off', 'para_on', 'orig_on', 'orig_off')
-results %<>% as.data.frame() %>% as_tibble()
-results[,'time'] <- 1:nrow(results)
-results[,'paramset'] <- i
-
-# sample every 300 timesteps for pseudo-single-cells pre-mutation (0-10k - "pre") and post-mutation (10k-20k "post imm"; 20k-30k "post lat")
-
-
-# calculate gini
-
-
-# store
-
+  results <- t(as.matrix(read.csv('S_outpar_test_ifs.csv', header=F)))
+  colnames(results) <- c('orig_1', 'para_1', 'targ_1', 'targ_off', 'targ_on', 'para_off', 'para_on', 'orig_on', 'orig_off')
+  results %<>% as.data.frame() %>% as_tibble()
+  results[,'time'] <- 1:nrow(results)
+  results[,'paramset'] <- i
+  results %<>% filter(time <= 30000)
+  
+  # sample every 300 timesteps for pseudo-single-cells pre-mutation (0-10k - "pre") and post-mutation (10k-20k "post imm"; 20k-30k "post lat")
+  
+  
+  # calculate CV and gini
+  
+  
+  # store
+  
 }
+
+# plot CV and gini values for each parameter set
+
+
+
+# isolate any parameter sets with weird values
+
+
 orig_color = 'black'
 para_color = 'gray'
 targ_color = 'blue'
